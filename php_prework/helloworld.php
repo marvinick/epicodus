2e251e46740a0fde86fadfed730c9ffb6e4ed5ca
marvinick <marviluw@gmail.com>
2015-07-20 12:24:45 -0700

    phpprework..Exercises: Meet the server

---
+++ php_prework/helloworld.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World!</title>
+    <head> <link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
